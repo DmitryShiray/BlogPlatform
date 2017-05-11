47cf1b99e6aaeb49285aed077eaf90ac3e166b37
Dmitry Shiray <dimitry.shiray@tut.by>
2017-05-11 10:05:47 +0300

    Fixed issue with authenticated user check. Added html binding for article content.

--- BlogPlatform.Infrastructure/Constants/Claims.cs
+++ BlogPlatform.Infrastructure/Constants/Claims.cs
@@ -2,7 +2,7 @@
 {
     public class Claims
     {
-        public const string ClaimsPolicyValue = "EmailAddress";
+        public const string ClaimsPolicyValue = "AuthorizedUser";
         public const string ClaimsPolicyName = "AuthorizedUser";
         public const string ClaimsAutorizedRole = "AuthorizedUser";
     }
